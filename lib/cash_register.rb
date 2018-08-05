9e172a437e055fc8e5016bcb645399479fc22fd9
Juan Tenezaca <tenj34@gmail.com>
2018-08-05 16:40:39 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,6 +14,17 @@ def add_item(title,price,quantity =1)
   end
   @last_transaction = quantity * amount
 end
+def apply_discount
+  if @discount == 0
+    return "There is no discount to apply."
+  else
+    @total = @total * ((100.0 - discount) / 100.0)
+    return "After the discount, the total comes to $#{@total.to_i}."
+  end
+end
+ def void_last_transaction
+  @total = @total - @last_transaction
+end
 
 
 
