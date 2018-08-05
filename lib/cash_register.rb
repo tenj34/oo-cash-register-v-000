6e626225542287372bcf3e91f4e04f3b871dcc97
Juan Tenezaca <tenj34@gmail.com>
2018-08-05 16:42:50 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,11 +1,11 @@
 class CashRegister
   attr_accessor :total, :discount, :items, :last_transaction
 
-def initialize(discount = 0)
-  @total = 0
-  @discount = discount
-  @items = [] # An empty of array of items everytime we call the class
-end
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = [] # An empty of array of items everytime we call the class
+  end
 
   def add_item(title,price,quantity = 1)
     self.total += quantity * amount
